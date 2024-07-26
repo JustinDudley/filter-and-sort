c709131b05f519b4e7e67da535c79180f9fb3cd4
Justin Dudley <justindudley99@gmail.com>
2024-07-26 17:04:09 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# with some trepidation, I begin a third project
+# I know that "doing things the right way" is best, but at least for a moment, I'm going to forger ahead
+# and try to get this job done!
+
+# It would be great to go ahead and develop all my "bad factors" ideas. But I'm just not sure it's necessary.
+
+print("Hello there worldy world")
