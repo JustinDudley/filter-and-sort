c9504c49b9b920fd66ea24276269c05a04c95fc2
Justin Dudley <justindudley99@gmail.com>
2024-07-26 17:15:18 -0400

    read and write files

--- main.py
+++ main.py
@@ -4,4 +4,31 @@
 
 # It would be great to go ahead and develop all my "bad factors" ideas. But I'm just not sure it's necessary.
 
-print("Hello there worldy world")
+
+
+
+
+import datetime
+
+
+startTime = datetime.datetime.now() # to monitor performance of program
+
+with open("/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/filter-and-sort/INPUT_file/final_alg_list_input.txt") as file_input:
+    final_algs = file_input.read().splitlines() 
+
+
+
+print("Hello there worldy world\n")
+print(final_algs)
+
+
+# WRITE TO FILE
+dt = datetime.datetime.now()
+output_filename = '/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/filter-and-sort/OUTPUT_files/%s.txt'%(dt.strftime("%a") + "_" + dt.strftime("%I") + ":" + dt.strftime("%M") + ":" + dt.strftime("%S") + "_output")
+with open(output_filename, "x") as output_file:
+	for alg in final_algs:
+		output_file.write(f"{alg}\n")
+
+
+
+print("time elapsed: ", datetime.datetime.now() - startTime)
