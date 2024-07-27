3fda930fb15b3b8e86da1e2fdd38bd24b27317b7
Justin Dudley <justindudley99@gmail.com>
2024-07-27 00:01:03 -0400

    No tri-turns, no S except RSR and LSL

--- methods/is_bad_pattern_when_capitalized.py
+++ methods/is_bad_pattern_when_capitalized.py
@@ -10,10 +10,23 @@ def is_bad_pattern_when_capitalized(alg):
 
     print(alg)
 
-    for bad_pattern in ["RT", "TR", "LT", "TL", "FT", "TF", "ST", "TS", "BT", "TB", "HT", "TH", "DT", "TD"]:
+    #TX, XT, TY, YT, TZ, Zt are trickier because WCR ARE okay at the beginning or end with a T
+
+
+
+
+    for bad_pattern in ["HT", "TH", "DT", "TD", "FT", "TF", "ST", "TS", "BT", "TB"]:    # left out RT, TR, LT, TL because their presence would indicate a larger problem, one that I'd like to know about
         if alg.find(bad_pattern) >= 0:
             return True
-        
+   
+
+    for bad_pattern in ["TS", "ST", "US", "SU", "HS", "SH", "DS", "SD"]:    # LS is okay      # left out FS, SF, BS, SB because their presence would indicate a larger problem, one that I'd like to know about
+        if alg.find(bad_pattern) >= 0:
+            return True  
+   
+    for bad_pattern in ["RR", "LL", "UU", "DD", "FF", "BB", "RL", "LR", "UD", "DU", "FB", "BF"]:     # tri-turns
+        if alg.find(bad_pattern) >= 0:
+            return True
         
     # needs testing!!
 
