63fe082b0b25cc3c258df69e47a392a4a11f1c27
Justin Dudley <justindudley99@gmail.com>
2025-07-31 09:40:00 -0400

    change INPUT file name

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ this_combo_is_trash__2__corner_rL_over17 = False
 
 
 startTime = datetime.datetime.now()
-with open("/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/filter-and-sort/INPUT_file/final_alg_list_input.txt") as file_input:
+with open("/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/filter-and-sort/INPUT_file/INPUT__pythonWidened_finished_algs.txt") as file_input:
     algs = file_input.read().splitlines() 
 
 pattern, group_number, isCornerAlg = identify_alg(algs[0])
