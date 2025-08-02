2e673e47510ddc3654deab743e8dd7d9ec604cf4
Justin Dudley <justindudley99@gmail.com>
2025-08-02 10:16:42 -0400

    add internal XS and SX to not-allowed in obligatory filters

--- methods__filtering/contains_internal_SY_or_ZS.py
+++ methods__filtering/contains_internal_SY_or_ZS.py
@@ -8,19 +8,19 @@ def contains_internal_SY_or_ZS(alg):
 
 
 
-    for leading_YorZ_substring in ["YS", "ZS"]:
+    for leading_YorZ_substring in ["XS", "YS", "ZS"]:
         if alg.find(leading_YorZ_substring) == 0:   # if True, then alg DOES begin with YS or ZS, so it contains no internal YorZ, and the alg is acceptable
             return False
 
 
-    for trailing_YorZ_substring in ["SY", "SZ"]:
+    for trailing_YorZ_substring in ["SX", "SY", "SZ"]:
         if alg.find(trailing_YorZ_substring) >= 0:   # check substring exists. rindex() throws error if substring not there
             if alg.rindex(trailing_YorZ_substring) == len(alg) - 2:   #rindex() gives final index of a given substring
                 return False   # if alg ends with SY or SZ, it's acceptable. Remove it from consideration for being filtered out. It doesn't have an INTERNAL SY or SZ.       
             
        
 
-    for internal_YorZ_substring in ["YS", "ZS", "SY", "SZ"]:
+    for internal_YorZ_substring in ["XS", "YS", "ZS", "SX", "SY", "SZ"]:   # not acceptable
         if alg.find(internal_YorZ_substring) >= 0:
             return True
         
