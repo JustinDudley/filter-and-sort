358ef3785d9cc15546bff5c55945f6ca537a28df
Justin Dudley <justindudley99@gmail.com>
2025-12-05 00:01:50 -0500

    replace absolute filepaths with local

--- variables/dataframes.py
+++ variables/dataframes.py
@@ -1,3 +1,3 @@
 import pandas as pd 
 
-df_stickers_turned = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/filter-and-sort/tables/stickers_turned_by_alg_notation.csv', index_col="orig") 
+df_stickers_turned = pd.read_csv('tables/stickers_turned_by_alg_notation.csv', index_col="orig") 
